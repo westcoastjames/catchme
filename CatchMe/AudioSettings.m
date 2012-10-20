13b20da2e0b860b4d6cbefea64224e6776b30aad
Nicholas Hoekstra <nhoekstr@sfu.ca>
2012-10-20 16:51:32 -0700

    Audio settings work in progress.

---
+++ CatchMe/AudioSettings.m
@@ -0,0 +1,13 @@
+//
+//  AudioSettings.m
+//  CatchMe
+//
+//  Created by Nicholas Hoekstra on 10/20/12.
+//  Copyright (c) 2012 Same Level Software. All rights reserved.
+//
+
+#import "AudioSettings.h"
+
+@implementation AudioSettings
+
+@end

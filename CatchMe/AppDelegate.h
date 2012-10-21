c75f8996aa78ac3349104620eecf23462d3dbc3e
Jonathon Simister-Jennings <jsimiste@infocmns07.ucslabs.sfu.ca>
2012-10-20 17:49:38 -0700

    Added CoreMotion include + tried to fix James' error

--- CatchMe/AppDelegate.h
+++ CatchMe/AppDelegate.h
@@ -2,13 +2,19 @@
 //  AppDelegate.h
 //  CatchMe
 //
-//  Created by Jonathon Simister-Jennings on 10/17/12.
+//  Created by Jonathon Simister on 10/17/12.
 //  Copyright (c) 2012 Same Level Software. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+#import <CoreMotion/CoreMotion.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate> {
+    
+    @public
+    CMMotionManager* motionManager;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 

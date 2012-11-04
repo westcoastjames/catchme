82d7bf69faeca358ec277d1e243b7f4346809b33
Brian Mo <bym@infocmns06.ucslabs.sfu.ca>
2012-11-03 21:19:38 -0700

    added pitch, roll, yaw data readings

--- CatchMe/MainMenuViewController.m
+++ CatchMe/MainMenuViewController.m
@@ -23,11 +23,20 @@
     }
     return self;
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    motionManager = [[CMMotionManager alloc] init];
+    motionManager.deviceMotionUpdateInterval = 0.05; // 20 Hz
+    
+    NSLog(@"Pitch = %.02f, Roll = %.02f, Yaw = %.02f", motionManager.deviceMotion.attitude.pitch, motionManager.deviceMotion.attitude.roll, motionManager.deviceMotion.attitude.yaw);
+    
+}
 							
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
     
     
 }
